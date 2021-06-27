90d9ea5305951502ed7515206501c068f191848e
Alexander Dedykhin <vyshky2@gmail.com>
2021-06-27 20:37:47 +0400

    DONE

--- Palindrom/Program.cs
+++ Palindrom/Program.cs
@@ -4,9 +4,20 @@ namespace Palindrom
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string word;
+            word = Console.ReadLine();
+
+            Console.WriteLine($"Слово {word} {(IsPalindrom(word)? "": "НЕ ")} ПАЛИНДРОМ");
+        }
+        static bool IsPalindrom(string word)
+        {
+            for (int i = 0; i <= word.Length - i - 1; i++)
+            {
+                if (word[i] != word[word.Length - i - 1]) return false;
+            }
+            return true;
         }
     }
 }
