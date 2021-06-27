cdb7752ddd9f5298940d7ec220dc50d0bd5eecf9
Alexander Dedykhin <vyshky2@gmail.com>
2021-06-27 20:33:34 +0400

    Initial commit

---
+++ Palindrom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
